4f36dcdce09aae2ece52db525764708a39afca08
LIM KWANG KYU <kk.lim@gsshop.com>
2020-04-09 17:17:10 +0900

    feat : 19강 숙제까지 진행

--- src/App.js
+++ src/App.js
@@ -1,80 +1,107 @@
-import React, { useRef, useState, useMemo, useCallback, memo } from "react";
+import React, { useRef, useReducer, useMemo, useCallback } from "react";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 import "./App.css";
+import useInputs from './useInputs';
 
 function countActiveUsers(users) {
   let count = users.filter(user => user.active).length;
   console.log("활성화된 사용자 처리 : " + count);
   return count;
 }
+
+const initalState = {
+  users: [ {
+    id: 1,
+    username: "veroper",
+    email: "veroper@example.com"
+  },
+  {
+    id: 2,
+    username: "veroper",
+    email: "veroper@example.com"
+  },
+  {
+    id: 3,
+    username: "veroper",
+    email: "veroper@example.com"
+  }]
+}
+
+function reducer(state, action){
+  switch(action.type){
+    case 'CREATE_USER':
+      return {
+        inputs: initalState.inputs,
+        users: state.users.concat(action.user)
+      }
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user => 
+          user.id === action.id
+          ? { ...user, active: !user.ative}
+          :user)
+      }
+    case 'REMOVE_USER': 
+      return {
+        ...state,
+        users: state.users.filter(user=> user.id !== action.id)
+      }
+    default: 
+      throw new Error('Unhandled action');
+  }
+}
 const App = () => {
-  const [inputs, setInputs] = useState({
-    username: "",
-    email: ""
-  });
-  const { username, email } = inputs;
-  const onChagnge = useCallback(e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  }, [inputs]);
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      username: "veroper",
-      email: "veroper@example.com"
-    },
-    {
-      id: 2,
-      username: "veroper",
-      email: "veroper@example.com"
-    },
-    {
-      id: 3,
-      username: "veroper",
-      email: "veroper@example.com"
-    }
-  ]);
-
   const nextId = useRef(4);
-  const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    // setUsers([...users, user]);
-    setUsers(users => users.concat(user));
-    setInputs({
-      username: "",
-      email: ""
-    });
-    console.log(nextId.current);
+  const [state, dispatch] = useReducer(reducer, initalState);
+  const [form, onChange, reset] = useInputs({
+    username: '', 
+    email: ''
+  });
+  const { username, email } = form;
+  const { users } = state;
+
+  const onCreate = useCallback( () => {
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email
+      }
+    })
     nextId.current += 1;
-  }, [username, email, ]);
-  const onRemove = useCallback(id => {
-    setUsers(users => users.filter(user => user.id !== id));
+    reset();
+  }, [username, email, reset]);
+
+  const onToggle = useCallback( (id) => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    })
   }, []);
 
-  const onToggle = useCallback(id => {
-    setUsers(users => users.map(u => (u.id === id ? { ...u, active: !u.active } : u)));
+  const onRemove = useCallback( (id) => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    })
   }, []);
 
-  const count = useMemo(() => countActiveUsers(users), [users]);
+  const count = useMemo(() => countActiveUsers(state.users), [state.users]);
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChagnge}
-        onCreate={onCreate}
-      />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <CreateUser 
+        username={username} 
+        email={email} 
+        onChange={onChange} 
+        onCreate={onCreate} />
+      <UserList 
+        users={users}
+        onToggle={onToggle}
+        onRemove={onRemove} />
       <div>활성화된 사용자 수 : {count}</div>
     </div>
   );
