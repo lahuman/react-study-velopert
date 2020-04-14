b7f1a8d859cf12e185cc6efb6749d055dd09fb4d
LIM KWANG KYU <kk.lim@gsshop.com>
2020-04-14 18:27:01 +0900

    32강 sentry 연동

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ import './index.css';
 import App from './App';
 import ContextSample from './ContextSample';
 import Counter from './Counter';
+import User from './User';
+import ErrorBoundary from './ErrorBoundary';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({dsn: "https://26781886aee34eaca58c5fdc8aacc395@o377526.ingest.sentry.io/5199757"});
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <Counter />
-    <ContextSample />
+  {
+    // <App />
+    // <Counter />
+    // <ContextSample />
+  }
+  <ErrorBoundary>
+    <User />
+  </ErrorBoundary>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
