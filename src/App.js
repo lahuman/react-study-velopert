0a3fe9cf8f4fa86f77b9cd45e95a63c94b7a9ca9
LIM KWANG KYU <kk.lim@gsshop.com>
2020-04-14 17:52:16 +0900

    29강까지 진행

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { useRef, useReducer, useMemo, useCallback, createContext } from "
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 import "./App.css";
-import useInputs from './useInputs';
+import produce from 'immer';
+
+window.produce = produce;
 
 function countActiveUsers(users) {
   let count = users.filter(user => user.active).length;
@@ -31,23 +33,35 @@ const initalState = {
 function reducer(state, action){
   switch(action.type){
     case 'CREATE_USER':
-      return {
-        inputs: initalState.inputs,
-        users: state.users.concat(action.user)
-      }
+      return produce(state, draft => {
+        draft.users.push(action.user);
+        draft.input = initalState.inputs;
+      });
+      // return {
+      //   inputs: initalState.inputs,
+      //   users: state.users.concat(action.user)
+      // }
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(user => 
-          user.id === action.id
-          ? { ...user, active: !user.ative}
-          :user)
-      }
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      });
+      // return {
+      //   ...state,
+      //   users: state.users.map(user => 
+      //     user.id === action.id
+      //     ? { ...user, active: !user.ative}
+      //     :user)
+      // }
     case 'REMOVE_USER': 
-      return {
-        ...state,
-        users: state.users.filter(user=> user.id !== action.id)
-      }
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id !== action.id);
+        draft.users.splice(index, 1);
+      })
+      // return {
+      //   ...state,
+      //   users: state.users.filter(user=> user.id !== action.id)
+      // }
     default: 
       throw new Error('Unhandled action');
   }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import ContextSample from './ContextSample';
+import Counter from './Counter';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Counter />
+    <ContextSample />
   </React.StrictMode>,
   document.getElementById('root')
 );
